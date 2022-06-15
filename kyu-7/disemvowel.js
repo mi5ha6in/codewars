68e2ea4dc6eb485fa7a94c262027da1dbec60bf3
mi5ha6in <mi5ha6in@gmail.com>
2022-06-15 23:54:17 +0300

    resolve the task https://www.codewars.com/kata/disemvowel-trolls

---
+++ kyu-7/disemvowel.js
@@ -0,0 +1,19 @@
+// 7 kyu https://www.codewars.com/kata/disemvowel-trolls
+
+function disemvowel(str) {
+  //   create a set to check for the presence of a letter in O(1)
+  // object can be used
+
+  const vowels = new Set(["a", "e", "i", "o", "u"]);
+  const arrayFromStr = str.split("");
+
+  const stringWithoutVowels = [];
+
+  for (let symbol of arrayFromStr) {
+    if (!vowels.has(symbol.toLowerCase())) {
+      stringWithoutVowels.push(symbol);
+    }
+  }
+
+  return stringWithoutVowels.join("");
+}
